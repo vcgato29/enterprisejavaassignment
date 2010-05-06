d0f0b684be5fb7910675476343d34927afb226da
james_16_12_88 <james_16_12_88@hotmail.com>
2010-05-06 13:35:41 +0000

    fixed a few bugs refactored Account

--- SwinBank/SwinBank-ejb/src/java/swinbank/server/ejb/account/Account.java
+++ SwinBank/SwinBank-ejb/src/java/swinbank/server/ejb/account/Account.java
@@ -19,34 +19,34 @@ import swinbank.server.policy.SwinDatabase.User;
 @Remote(AccountRemote.class)
 public class Account implements AccountRemote {
 
-    public double getBalance(String accountId, String custID, ClientType clientType) throws AccessDeniedException {
+    public double getBalance(String accountId, String custId, ClientType clientType) throws AccessDeniedException {
 
-        //Call made to static method getbalance()
-        UserAccount acc = SwinDatabase.getAccount(accountId);
-        //need to check if the account is active
-        if (acc == null) {
+        //check if the  account exisis
+        if (!accountExists(accountId)) {
             throw new AccessDeniedException("\nAccount does not Exist!");
         }
 
-        if (acc.userId.equals(custID)) {
-            return acc.balance;
-        }
+        if (clientType == ClientType.IB || clientType == ClientType.ATM) {
+            //check if the customer owns the accounts
+            if (ownAccount(accountId, custId)) {
+                return SwinDatabase.account(accountId).balance;
+            } else {
+                throw new AccessDeniedException("\nYou do not own of the Account!");
+            }
+        } else {//TM
 
-        if (clientType == ClientType.TM) {
-            return acc.balance;
-        } else {
-            throw new AccessDeniedException("\nClient does not have enough priviliges to perform this action!");
+            return SwinDatabase.account(accountId).balance;
         }
 
     }
 
     //current Customer
-    public void createAccount(String custID, ClientType clientType, AccountType accountType) throws AccessDeniedException {
+    public void createAccount(String custId, ClientType clientType, AccountType accountType) throws AccessDeniedException {
         if (clientType == ClientType.TM) {
             //check that the customer exist and isnt staff
-            if (CheckCustomerID(custID)) {
+            if (checkCustomerID(custId)) {
                 //Call the Create Account method in the database it will need to take the CumtomerID and the AccountType
-                SwinDatabase.CreateAccount(custID, accountType);
+                SwinDatabase.createAccount(custId, accountType);
             } else {
                 throw new AccessDeniedException("\nCan not create an account for that Customer!");
             }
@@ -56,7 +56,7 @@ public class Account implements AccountRemote {
     }
 
     //create an account for a new customer
-    public void createAccount(ClientType clientType, AccountType accountType) throws AccessDeniedException {
+    public void createAccountNewCustomer(ClientType clientType, AccountType accountType) throws AccessDeniedException {
 
         if (clientType == ClientType.TM) {
             //Call the Create Account method in the database it will need to take the AccountType
@@ -66,31 +66,14 @@ public class Account implements AccountRemote {
         }
     }
 
-    public void removeAccount(String custID, String accountId, ClientType clientType) throws AccessDeniedException {
+    public void removeAccount(String accountId, ClientType clientType) throws AccessDeniedException {
         //check if its a TM
         if (clientType == ClientType.TM) {
-
-            UserAccount acc = SwinDatabase.getAccount(accountId);
-
-            //check if the account exisis
-            if (acc == null) {
+            //check if the  account exisis
+            if (!accountExists(accountId)) {
                 throw new AccessDeniedException("\nAccount does not Exist!");
             }
-
-            //check that the customer exist and isnt staff
-            if (CheckCustomerID(custID)) {
-                //check if the customer has any accounts
-                if (getAccounts(custID).size() == 1) {
-                    SwinDatabase.SetAccountInactive(accountId);
-                    SwinDatabase.SetCustomerInactive(custID);
-                } else {
-                    SwinDatabase.SetAccountInactive(accountId);
-                }
-
-
-            } else {
-                throw new AccessDeniedException("\nCan not remove a Account from that Customer");
-            }
+            SwinDatabase.SetAccountInactive(accountId);
         } else {
             throw new AccessDeniedException("\nClient does not have enough priviliges to perform this action!");
         }
@@ -99,15 +82,15 @@ public class Account implements AccountRemote {
 
     //returns the id of each accout in the list
     public List<String> getAccounts(String custID) throws AccessDeniedException {
-        if (CheckCustomerID(custID)) {
+        //check if customer exists
+        if (checkCustomerID(custID)) {
             return SwinDatabase.GetAccounts(custID);
         } else {
             throw new AccessDeniedException("\nCan not get the Accounts for that Customer");
         }
     }
 
-    public List<String> getTransactions(String accountId, String custID, ClientType clientType, Date fromDate, Date toDate) throws AccessDeniedException {
-
+    public List<String> getTransactions(String accountId, String custId, ClientType clientType, Date fromDate, Date toDate) throws AccessDeniedException {
         //check Data errors
         if (toDate.after(new Date())) {
             throw new AccessDeniedException("\nTo Data can not be in the future");
@@ -116,34 +99,47 @@ public class Account implements AccountRemote {
             throw new AccessDeniedException("\nFrom Data can not be in the future");
         }
 
-
-        //Call made to static method getbalance()
-        UserAccount acc = SwinDatabase.getAccount(accountId);
-        //need to check if the account is active
-
-        //check if the account exisis
-        if (acc == null) {
+        //check if the  account exisis
+        if (!accountExists(accountId)) {
             throw new AccessDeniedException("\nAccount does not Exist!");
         }
 
-        //check that the account is owned by the customer
-        if (acc.userId.equals(custID)) {
+        if (clientType == ClientType.IB || clientType == ClientType.ATM) {
+            //check if the customer owns the accounts
+            if (ownAccount(accountId, custId)) {
+                return SwinDatabase.GetTransactions(accountId, fromDate, toDate);
+            } else {
+                throw new AccessDeniedException("\nYou do not own of the Account!");
+            }
+        } else {//TM
             return SwinDatabase.GetTransactions(accountId, fromDate, toDate);
-        }
-
-        if (clientType == ClientType.TM) {
-            return SwinDatabase.GetTransactions(accountId, fromDate, toDate);
-        } else {
-            throw new AccessDeniedException("\nClient does not have enough priviliges to perform this action!");
         }
     }
 
-    private Boolean CheckCustomerID(String custID) {
+    private Boolean checkCustomerID(String custID) {
         User user = new SwinDatabase().getUser(custID);
         if (user == null) {
             return false;
         }
         if (user.isStaff) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean ownAccount(String accountId, String custId) {
+        UserAccount acc = SwinDatabase.getAccount(accountId);
+        if (acc.userId.equals(custId)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean accountExists(String accountId) {
+        UserAccount acc = SwinDatabase.getAccount(accountId);
+        //check if the account exisis
+        if (acc == null) {
             return false;
         }
         return true;
