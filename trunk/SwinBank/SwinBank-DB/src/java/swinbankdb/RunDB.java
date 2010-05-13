868a179a58506ef1ea5bfed54105053a9335d5f8
vipin.aupp@gmail.com <vipin.aupp@gmail.com@c22853fd-ee17-6ca7-c517-497e0fa3e4a6>
2010-05-13 03:03:35 +0000

    New SwinBankDB Project.......has been created

---
+++ trunk/SwinBank/SwinBank-DB/src/java/swinbankdb/RunDB.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swinbankdb;
+
+/**
+ *
+ * @author Vipin
+ * @version 1.0
+ */
+public class RunDB {
+
+    public static void main(String[] args) {
+        DB db = new DB();
+        db.createTable();
+    }
+}
