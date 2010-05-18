e5c1a98f081d3baf5528c79efe239320c2c890be
james_16_12_88@hotmail.com <james_16_12_88@hotmail.com@c22853fd-ee17-6ca7-c517-497e0fa3e4a6>
2010-05-18 05:37:49 +0000

    Removed the errors in AccountBean
    Still need to fix the transactions finding bit but apart from that its fine

--- trunk/SwinBank/SwinBank-ejb/src/java/swinbank/server/ejb/account/AccountRemote.java
+++ trunk/SwinBank/SwinBank-ejb/src/java/swinbank/server/ejb/account/AccountRemote.java
@@ -4,6 +4,8 @@ import java.rmi.RemoteException;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.Remote;
+import swinbank.server.entity.Account;
+import swinbank.server.entity.Transactions;
 import swinbank.server.policy.AccountType;
 import swinbank.server.policy.ClientType;
 
@@ -16,8 +18,8 @@ public interface AccountRemote extends java.rmi.Remote{
 
     double getBalance(int accountId, int userId, ClientType clientType) throws RemoteException;
     void createAccount(int custID, ClientType clientType, AccountType accountType) throws RemoteException;
-    //void createAccountNewCustomer(ClientType clientType, AccountType accountType) throws RemoteException;
+    void createAccountNewCustomer(ClientType clientType, AccountType accountType, String password) throws RemoteException;
     void removeAccount(int accountId, ClientType clientType) throws RemoteException;
-    List<String> getAccounts(int custId) throws RemoteException;
-    List<String> getTransactions(int accountId, int custId, ClientType clientType, Date fromDate, Date toDate) throws RemoteException;
+    List<Account> getAccounts(int custId) throws RemoteException;
+    List<Transactions> getTransactions(int accountId, int custId, ClientType clientType, Date fromDate, Date toDate) throws RemoteException;
 }
