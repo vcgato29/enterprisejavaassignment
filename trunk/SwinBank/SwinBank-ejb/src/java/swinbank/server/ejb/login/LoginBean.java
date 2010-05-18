498a526ff4da661266ce3042b4ad809d4667b4e2
james_16_12_88@hotmail.com <james_16_12_88@hotmail.com@c22853fd-ee17-6ca7-c517-497e0fa3e4a6>
2010-05-18 10:11:52 +0000

    fixed login

--- trunk/SwinBank/SwinBank-ejb/src/java/swinbank/server/ejb/login/LoginBean.java
+++ trunk/SwinBank/SwinBank-ejb/src/java/swinbank/server/ejb/login/LoginBean.java
@@ -4,12 +4,13 @@ import java.rmi.RemoteException;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.rmi.PortableRemoteObject;
+import swinbank.server.entity.Login;
+import swinbank.server.policy.AccessDeniedException;
 import swinbank.server.policy.ClientType;
-import swinbank.server.policy.SwinDatabase;
-import swinbank.server.policy.SwinDatabase.User;
 
 /**
  *
@@ -22,31 +23,46 @@ public class LoginBean extends PortableRemoteObject implements LoginRemote {
     @PersistenceContext
     private EntityManager em;
 
-    public LoginBean() throws RemoteException{
+    public LoginBean() throws RemoteException {
+    }
+
+    public boolean login(int custId, String password, ClientType clientType) throws RemoteException {
+        Login login = null;
+        //check if the  login exisis
+        try {
+            login = getLogin(custId);
+        } catch (NoResultException e) {
+            throw new AccessDeniedException("\nUser does not exist");
+        }
+        if (login.getPassword().equals(password)) {
+            if (clientType == ClientType.TM) {
+                if (login.getIsstaff() == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                //check if they have accounts
+                Query countQuery = em.createNamedQuery("Login.accountsCount").setParameter("userid", custId);
+                int count = (Integer) countQuery.getSingleResult();
+                if (count > 0) {
+                    if (login.getIsstaff() == 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+
+        return false;
 
     }
 
-    public boolean login(String userId, String password, ClientType clientType) throws RemoteException{
-
-        User user = new SwinDatabase().getUser(userId);
-        Query queryGetUser = em.createNamedQuery("Login.findByCustid").setParameter("custid", userId);
-        if (user.password.equals(password))
-        {
-            if(clientType == ClientType.TM)
-            {
-                return user.isStaff;
-            }
-            else
-            {
-                //check if they have accounts
-                Query countQuery = em.createNamedQuery("Login.accountsCount").setParameter("userid", user.userId);
-                int count =(Integer) countQuery.getSingleResult();
-                if (count > 0)
-                    return !user.isStaff;
-            }
-        }
-        return false;
-        
-
+    private Login getLogin(int userId) {
+        Query userByLoginId = em.createNamedQuery("Login.findByCustid").setParameter("custid", userId);
+        Login login = (Login) userByLoginId.getSingleResult();
+        return login;
     }
 }
